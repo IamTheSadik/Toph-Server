b56f27ba0a50c4c55a94fb8550b3a34b540748c0
Nusab19 <nusabtaha33@gmail.com>
2023-04-22 00:44:06 +0600

    ok ig

--- main.py
+++ main.py
@@ -23,11 +23,13 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:5000",
+    "http://localhost:5500",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
