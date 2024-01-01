c37443b70d31d9817e87ce116b9fc2a3aa3daf08
Nusab Taha <nusabtaha33@gmail.com>
2024-01-01 07:30:57 +0600

    first send message

--- test.py
+++ test.py
@@ -1,30 +1,9 @@
 from bs4 import BeautifulSoup
 import httpx
 
-import json
-import time
-import asyncio
+url = "https://nusab19.pages.dev/api/sendMessage"
 
-
-url = "https://toph.co/p/all"
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"
-}
-
-async def req(url, ses):
-    r = await ses.get(url, headers=headers, follow_redirects=1)
-    return r
-
-async def main():
-    ses = httpx.AsyncClient(
-        timeout=10, headers=headers, follow_redirects=1)
-
-    r = await asyncio.gather(*[req(url, ses) for _ in range(100)], return_exceptions=True)
-    for i in r:
-        print(i)
-    
-    await ses.aclose()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+httpx.post(url, json={
+    "name": "github actions",
+    "message": "check this out!"
+})
