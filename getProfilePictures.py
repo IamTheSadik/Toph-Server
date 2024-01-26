f9ac031802306dad17dd3c528f1bf933e0a1b0ae
Nusab Taha <nusabtaha33@gmail.com>
2024-01-26 07:50:04 +0600

    Refactor getProfilePictures.py: Import modules, add text overlay to error images

--- getProfilePictures.py
+++ getProfilePictures.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
-import httpx, time, os, json
+import httpx
+import os
+import json
+from PIL import Image, ImageDraw, ImageFont
 
 if not os.path.exists("Data/profilePictures"):
     os.makedirs("Data/profilePictures/compressed")
@@ -16,6 +19,14 @@ for x, y in leaderboard.items():
     for z in y:
         usernames.add(z)
 
+
+def textsize(text, font):
+    im = Image.new(mode="P", size=(0, 0))
+    draw = ImageDraw.Draw(im)
+    _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
+    return width, height
+
+
 for i, user in enumerate(sorted(usernames), 1):
     print(f"Getting @{user}      |       {i}/{len(usernames)}")
 
@@ -26,9 +37,22 @@ for i, user in enumerate(sorted(usernames), 1):
         img = soup.find("img", class_="d-block mw-100 h-auto")["src"]
     except TypeError:
         print(f"Error: {user}")
+        img = Image.new("RGB", (450, 450), color=(0, 102, 255))
+        d = ImageDraw.Draw(img)
+
+        fnt = ImageFont.truetype("Data/Fonts/Roboto-Regular.ttf", 200)
+        text = user[0].upper()
+        text_width, text_height = textsize(text, fnt)
+
+        # Calculate the x and y coordinates
+        x = (img.width - text_width) / 2
+        y = (img.height - text_height) / 2
+
+        # Draw the text
+        d.text((x, y), text, font=fnt, fill=(255, 255, 255))
+
+        img.save(f"Data/profilePictures/{user}.jpg", "JPEG", quality=100, optimize=False, progressive=False)
         continue
 
     with open(f"Data/profilePictures/{user}.jpg", "wb") as f:
         f.write(ses.get(img).content)
-
-
