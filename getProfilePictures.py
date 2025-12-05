89e7b3139e29afad08ce4b0f7fa19ab7b6ef9cb7
Nusab Taha <nusabtaha33@gmail.com>
2025-12-06 00:58:25 +0600

    photos

--- getProfilePictures.py
+++ getProfilePictures.py
@@ -1,12 +1,6 @@
-import os
 import json
 import httpx
 from bs4 import BeautifulSoup
-from PIL import Image, ImageDraw, ImageFont
-
-if not os.path.exists("Data/profilePictures"):
-    os.makedirs("Data/profilePictures/compressed")
-
 
 ses = httpx.Client(timeout=30, follow_redirects=1)
 
@@ -20,43 +14,19 @@ for x, y in leaderboard.items():
         usernames.add(z)
 
 
-def textsize(text, font):
-    im = Image.new(mode="P", size=(0, 0))
-    draw = ImageDraw.Draw(im)
-    _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
-    return width, height
 
-
+data = {}
 for i, user in enumerate(sorted(usernames), 1):
-    print(f"Getting @{user}      |       {i}/{len(usernames)}", end="\r")
-
+    print(f"Getting @{user}      |       {i}/{len(usernames)}", " "*30, end="\r")
     url = f"https://toph.co/u/{user}"
-    r = ses.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
     try:
-        img = soup.find("img", class_="d-block mw-100 h-auto")["src"]
-    except TypeError:
-        print(f"Error: {user}")
-        img = Image.new("RGB", (450, 450), color=(0, 102, 255))
-        d = ImageDraw.Draw(img)
+        res = ses.get(url)
+        doc = BeautifulSoup(res.content, "lxml")
+        img = doc.find("div", class_="avatar").find("img")["src"]
+        data[user] = img
+    except Exception as e:
+        print(f"Failed for {user}", e)
 
-        fnt = ImageFont.truetype("Data/Fonts/Roboto-Regular.ttf", 200)
-        text = user[0].upper()
-        text_width, text_height = textsize(text, fnt)
 
-        # Calculate the x and y coordinates
-        x = (img.width - text_width) / 2
-        y = (img.height - text_height) / 2
-
-        # Draw the text
-        d.text((x, y), text, font=fnt, fill=(255, 255, 255))
-
-        img.save(f"Data/profilePictures/{user}.jpg", "JPEG", quality=100, optimize=False, progressive=False)
-        continue
-
-    with open(f"Data/profilePictures/{user}.jpg", "wb") as f:
-        f.write(ses.get(img).content)
-
-print()
-
-import compressProfilePictures
+with open("Data/profilePhotos.json", 'w', encoding="utf8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
--- test.py
+++ test.py
@@ -1,31 +1,62 @@
-import random, httpx
+import os
+import json
+import httpx
+from bs4 import BeautifulSoup
+from PIL import Image, ImageDraw, ImageFont
+
+if not os.path.exists("Data/profilePictures"):
+    os.makedirs("Data/profilePictures/compressed")
 
 
-def generate_random_ip():
-    return ".".join(str(random.randint(0, 255)) for _ in range(4))
+ses = httpx.Client(timeout=30, follow_redirects=1)
+
+with open("Data/leaderboard.json", "rb") as f:
+    leaderboard = json.load(f)
+
+usernames = {"Safin01"}
+
+for x, y in leaderboard.items():
+    for z in y:
+        usernames.add(z)
 
 
-def getHeader():
-    yo = generate_random_ip()
-    return {
-        "X-Originating-IP": yo,
-        "X-Forwarded-For": yo,
-        "X-Remote-IP": yo,
-        "X-Remote-Addr": yo,
-        "X-Client-IP": yo,
-        "X-Host": yo,
-        "X-Forwared-Host": yo
-    }
+def textsize(text, font):
+    im = Image.new(mode="P", size=(0, 0))
+    draw = ImageDraw.Draw(im)
+    _, _, width, height = draw.textbbox((0, 0), text=text, font=font)
+    return width, height
 
 
-url = "https://httpbin.org/ip"
-url = "https://toph-api.onrender.com/ip"
-# url = "https://toph.co"
+for i, user in enumerate(sorted(usernames), 1):
+    print(f"Getting @{user}      |       {i}/{len(usernames)}", end="\r")
 
-ses = httpx.Client(timeout=None, follow_redirects=1)
+    url = f"https://toph.co/u/{user}"
+    r = ses.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    try:
+        img = soup.find("img", class_="d-block mw-100 h-auto")["src"]
+    except TypeError:
+        print(f"Error: {user}")
+        img = Image.new("RGB", (450, 450), color=(0, 102, 255))
+        d = ImageDraw.Draw(img)
 
+        fnt = ImageFont.truetype("Data/Fonts/Roboto-Regular.ttf", 200)
+        text = user[0].upper()
+        text_width, text_height = textsize(text, fnt)
 
-for i in range(10):
-    a = ses.get(url, headers=getHeader()).text
-    print(a)
-    break
+        # Calculate the x and y coordinates
+        x = (img.width - text_width) / 2
+        y = (img.height - text_height) / 2
+
+        # Draw the text
+        d.text((x, y), text, font=fnt, fill=(255, 255, 255))
+
+        img.save(f"Data/profilePictures/{user}.jpg", "JPEG", quality=100, optimize=False, progressive=False)
+        continue
+
+    with open(f"Data/profilePictures/{user}.jpg", "wb") as f:
+        f.write(ses.get(img).content)
+
+print()
+
+import compressProfilePictures
